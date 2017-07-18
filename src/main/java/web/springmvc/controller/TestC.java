2631b891387c853de26fe56ad9dca6128b2d5a0a
lwz <wenzhong.li@dmall.com>
2017-07-18 17:35:26 +0800

    pom

--- src/main/java/web/springmvc/controller/TestC.java
+++ src/main/java/web/springmvc/controller/TestC.java
@@ -1,10 +1,17 @@
 package web.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller(value = "test")
 public class TestC
 {
-	
+	@RequestMapping(value = "test")
+	@ResponseBody
+	public String test(){
+
+		return "w success";
+	}
 }
  
