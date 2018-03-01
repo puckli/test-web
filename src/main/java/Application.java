18f0579b24e60df340d3bc94f0f5c7aebc63199f
puck <liwenzhong@outlook.com>
2018-03-02 00:20:52 +0800

    java config replace web.xml

---
+++ src/main/java/Application.java
@@ -0,0 +1,36 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * replace web.xml
+ * @author puck
+ * @Description
+ * @Date 2018/3/1
+ */
+public class Application extends AbstractAnnotationConfigDispatcherServletInitializer{
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
+
+
+/**
+ <jsp-config>
+ <jsp-property-group>
+ <description>HTML Encoding</description>
+ <display-name>HTML Encoding Config</display-name>
+ <url-pattern>*.jsp</url-pattern>  <!-- 设定值所影响的范围，如：/CH2 或 /*.jsp -->
+ <url-pattern>*.html</url-pattern>
+ <el-ignored>false</el-ignored>   <!-- 若为true，表示不支持EL语法 -->
+ <page-encoding>UTF-8</page-encoding>
+ <scripting-invalid>false</scripting-invalid>  <!-- 若为true，表示不支持<% scripting %>语法 -->
+ </jsp-property-group>
+ </jsp-config>
+ */
